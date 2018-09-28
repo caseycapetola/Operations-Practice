6a6a4c5ae30ace4c4721cf2e5ffdc9af1c873c04
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2018-09-28 12:27:36 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,22 @@
+
+public class OperationsTest 
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Declaration statement
+		number = 20; //Assignment statement -> initialization
+		number+=5; //Spaces do not matter; could also do number = number + 5 for this line since it is read r -> l
+		System.out.println(number);
+		number/=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number %= 2;
+		System.out.println(number);
+
+	}
+
+}
